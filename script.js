0bc1be0a58b6617a4afe449f1a10ff08443d5ccb
sofija8 <66820942+sofija8@users.noreply.github.com>
2021-04-02 02:52:02 +0200

    5. Sort and Star

--- script.js
+++ script.js
@@ -97,4 +97,20 @@ function zeros(n) {
     return sum;
 }
 
-// 5. 
+// 5. Sort and Star
+/*
+You will be given a vector of strings. 
+You must sort it alphabetically 
+(case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+Example
+["take", "over", "the", "world", "maybe", "bitcoin", "who", "knows", "perhaps"] => 'b***i***t***c***o***i***n'
+*/
+
+function twoSort(s) {
+    return s.sort()[0].split("").join("***");
+}
